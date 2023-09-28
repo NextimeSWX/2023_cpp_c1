d4bc1297c85962f7cd2c24e55d5236e97c63e904
Evan Le Bihan <evan.lebihan@ecole-89.com>
2023-09-28 14:13:47 +0200

    chap

--- trd.cpp
+++ trd.cpp
@@ -5,17 +5,14 @@
 
 int verif(char token, std::string argv)
 {
-    int i = 0;
-
     auto iter{argv.begin()};
     while (iter != argv.end()) {
         if (token == *iter) {
-            return (i);
+            return (1);
         }
         iter += 1;
-        i += 1;
     }
-    return (-1);
+    return (0);
 }
 
 int main(int argc, char **argv)
@@ -24,23 +21,23 @@ int main(int argc, char **argv)
     int         j{0};
     std::string str;
 
-    if (argc!= 3) {
-        return (-1);
-    }
     while (std::getline(std::cin, str)) {
         auto iter{str.begin()};
-        i = 0;
+        if (argc == 2) {
             while (iter != str.end()) {
                 j = verif(*iter, argv[1]);
-                if (j >= 0) {
-                    str[i] = argv[2][j];
+                if (j == 1) {
+                    str.erase(iter);
                 } else {
                     str[i] = str[i];
+                    iter += 1;
+                    i += 1;
                 }
-                iter += 1;
-                i += 1;
             }
             std::cout << str << "\n";
+        } else {
+            return (-1);
+        }
     }
     return (0);
 }
